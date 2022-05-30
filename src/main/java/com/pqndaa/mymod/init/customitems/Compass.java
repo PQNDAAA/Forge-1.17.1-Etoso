aa44adbecc422e05b72a5d6d98c7c31885a79b9a
PQNDAAA <gayvallet.dylan@gmail.com>
2022-05-30 23:07:10 +0200

    Update Compass.java

--- src/main/java/com/pqndaa/mymod/init/customitems/Compass.java
+++ src/main/java/com/pqndaa/mymod/init/customitems/Compass.java
@@ -1,8 +1,12 @@
 package com.pqndaa.mymod.init.customitems;
 
+import com.pqndaa.mymod.init.world.biome.ModBiomes;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.data.worldgen.biome.Biomes;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
@@ -11,8 +15,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.minecraftforge.common.BiomeDictionary;
+
+import java.util.List;
+import java.util.Random;
 
 public class Compass extends Item {
 
@@ -27,23 +36,60 @@ public class Compass extends Item {
         ItemStack itemstack = p.getItemInHand(interactionhand);
         HitResult hitresult = getPlayerPOVHitResult(level, p, ClipContext.Fluid.SOURCE_ONLY);
         BlockPos blockpos = ((BlockHitResult)hitresult).getBlockPos();
+        Biome biome = level.getBiome(blockpos);
+
 
      //   if(p.level.isClientSide && p.getServer() != null){
        //     p.getServer().getCommands().performCommand(p.createCommandSourceStack().withSuppressedOutput().withPermission(4), "locatebiome mymod:lava_land");
         //}
 
 
-        System.out.println("cc " + getDistanceToEntity(p,blockpos));
+
+        System.out.println(locateBiome(level,biome,blockpos,6400,8));
 
         return InteractionResultHolder.success(itemstack);
 
     }
 
-    public static double getDistanceToEntity(Player p, BlockPos pos) {
+    public static double getDistanceToEntity(Player p,Level level,BlockPos pos) {
+
         double deltaX = p.position().x - pos.getX();
         double deltaY = p.position().y - pos.getY();
         double deltaZ = p.position().z - pos.getZ();
 
         return Math.sqrt((deltaX * deltaX) + (deltaY * deltaY) + (deltaZ * deltaZ));
     }
+
+    private static BlockPos locateBiome(Level level, Biome biome, BlockPos pos, int maxSearchRadius, int searchIncrement) {
+        return locateBiome(level, pos.getX(), pos.getY(), pos.getZ(), maxSearchRadius, searchIncrement, biome);
+    }
+
+    private static BlockPos locateBiome(Level level, int x, int y, int z, int maxSearchRadius, int Increment, Biome biome) {
+        int originX = x >> 2;
+        int originZ = z >> 2;
+        int border = maxSearchRadius >> 2;
+        int currentY = y >> 2;
+
+        BlockPos pos = null;
+        int idk;
+        for (idk = 0; idk <= border; idk += Increment) {
+            int zi;
+            for (zi = -idk; zi <= idk; zi += Increment) {
+                boolean flag1 = (Math.abs(zi) == idk);
+                int xi;
+                for (xi = -idk; xi <= idk; xi += Increment) {
+                    boolean flag2 = (Math.abs(xi) == idk);
+                    if (flag2 || flag1) {
+                        int currentX = originX + xi;
+                        int currentZ = originZ + zi;
+                        if(biome != ModBiomes.GRAVEYARD_LAND.get())
+                            System.out.println(new BlockPos(currentX << 2, y, currentZ << 2));
+                        return new BlockPos(currentX << 2, y, currentZ << 2);
+                    }
+                }
+            }
+        }
+        return pos;
+    }
+
 }
